8f7db70172b8a4eaeb4601c734f493fd32d5b9db
Gabe Womble <gwomble27@gmail.com>
2024-11-25 20:52:16 -0800

    Validating new trips

--- app/src/components/input.tsx
+++ app/src/components/input.tsx
@@ -7,7 +7,7 @@ type TextInputProps = {
   label: string;
   placeholder?: string;
   value?: string;
-  type?: "text" | "password";
+  type?: "text" | "password" | "textarea";
   fieldProps: FieldElementProps<Record<string, string>, string>;
   field: FieldStore<any, any>;
 };
@@ -18,15 +18,27 @@ export const TextInput = component$<TextInputProps>(
       <label for={id} class="mb-2 block text-sm font-bold text-base-content">
         {label}
       </label>
-      <input
-        {...fieldProps}
-        class={`input input-bordered w-full ${field.error ? "input-error" : ""}`}
-        type={type}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        value={field.value}
-      />
+
+      {type === "textarea" ? (
+        <textarea
+          {...fieldProps}
+          class={`input input-bordered w-full ${field.error ? "input-error" : ""}`}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          value={field.value}
+        />
+      ) : (
+        <input
+          {...fieldProps}
+          class={`input input-bordered w-full ${field.error ? "input-error" : ""}`}
+          type={type}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          value={field.value}
+        />
+      )}
       {field.error && <p class="my-1 text-error">{field.error}</p>}
     </>
   ),
