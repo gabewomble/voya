ff7aacce42657ab501c658a4d74276e46cb75e54
Gabe Womble <gwomble27@gmail.com>
2024-11-27 08:04:36 -0800

    update user and trip routes

--- app/src/routes/trips/[id]/layout.tsx
+++ app/src/routes/trips/[id]/layout.tsx
@@ -4,7 +4,7 @@ import { getTripByIdResponseSchema } from "~/types/api";
 
 export const useTripData = routeLoader$(async (requestEvent) => {
   const { id } = requestEvent.params;
-  const res = await serverFetch(`/trips/${id}`, {}, requestEvent);
+  const res = await serverFetch(`/trips/t/${id}`, {}, requestEvent);
   const json = await res.json();
   return getTripByIdResponseSchema.parse(json);
 });
