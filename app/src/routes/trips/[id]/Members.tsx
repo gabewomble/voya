613304a4005c7b0d65de057cc0624b91cea511ac
Gabe Womble <gwomble27@gmail.com>
2024-12-03 21:55:45 -0800

    remove the modular form for this page

--- app/src/routes/trips/[id]/Members.tsx
+++ app/src/routes/trips/[id]/Members.tsx
@@ -1,9 +1,7 @@
 import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import { useTripData } from "./layout";
-import { Card, CardTitle, TextInput } from "~/components";
+import { Card, CardTitle } from "~/components";
 import { server$, z } from "@builder.io/qwik-city";
-import { formAction$, useForm, zodForm$ } from "@modular-forms/qwik";
-import { useAddMemberLoader } from "./layout";
 import { isServer } from "@builder.io/qwik/build";
 import { serverFetch } from "~/helpers/server-fetch";
 import { searchUsersResponseSchema } from "~/types/api";
@@ -14,14 +12,6 @@ const addMemberFormSchema = z.object({
     .string()
     .min(4, "Username or email must be at least 4 characters long."),
 });
-
-export type AddMemberForm = z.infer<typeof addMemberFormSchema>;
-
-export const useAddMember = formAction$<AddMemberForm>(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async (data, request) => {},
-  zodForm$(addMemberFormSchema),
-);
 
 export const searchUsers = server$(async function (identifier: string) {
   if (!addMemberFormSchema.safeParse({ identifier }).success) {
@@ -49,80 +39,70 @@ export const searchUsers = server$(async function (identifier: string) {
 
 export const Members = component$(() => {
   const { members } = useTripData().value;
-  const [addMemberForm, { Form, Field }] = useForm({
-    loader: useAddMemberLoader(),
-    action: useAddMember(),
-    validateOn: "submit",
-    validate: zodForm$(addMemberFormSchema),
-  });
 
+  const userSearch = useSignal("");
   const searchTimeoutId = useSignal<number>();
   const userSuggestions = useSignal<User[]>([]);
 
   useTask$(async (ctx) => {
-    const id = ctx.track(() => addMemberForm.internal.fields.identifier?.value);
+    const searchValue = ctx.track(userSearch);
 
-    if (id !== undefined && !isServer) {
+    if (
+      !isServer &&
+      addMemberFormSchema.safeParse({ identifier: searchValue }).success
+    ) {
       window.clearTimeout(searchTimeoutId.value);
       searchTimeoutId.value = window.setTimeout(async () => {
         window.clearTimeout(searchTimeoutId.value);
-        userSuggestions.value = await searchUsers(id);
+        userSuggestions.value = await searchUsers(searchValue);
       }, 250);
+    } else {
+      userSuggestions.value = [];
     }
   });
 
   return (
     <Card>
       <CardTitle level={2}>Members</CardTitle>
-      <Form class="flex flex-col gap-4">
-        <div class="dropdown w-full">
-          <Field name="identifier">
-            {(field, props) => (
-              <TextInput
-                autocomplete="off"
-                id="identifier"
-                label="Search for member"
-                name="identifier"
-                placeholder="Enter username, name, or email"
-                field={field}
-                fieldProps={props}
-              />
-            )}
-          </Field>
-          {userSuggestions.value.length > 0 && (
-            <table class="table">
-              <caption>Search results</caption>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {userSuggestions.value.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>
-                      <button
-                        class="btn btn-primary btn-sm"
-                        type="button"
-                        // onClick={() => {
-                        //   addMemberForm.internal.fields.identifier?.setValue("");
-                        //   addMemberForm.submit({ identifier: user.id });
-                        // }}
-                      >
-                        Add
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-      </Form>
+      <label
+        class="mb-2 block text-sm font-bold text-base-content"
+        for="search-input"
+      >
+        Search
+      </label>
+      <input
+        autocomplete="off"
+        class="input input-bordered w-full"
+        type="text"
+        id="search-input"
+        placeholder="Enter username, name, or email"
+        bind:value={userSearch}
+      />
+      {userSuggestions.value.length > 0 && (
+        <table class="table">
+          <caption>Search results</caption>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {userSuggestions.value.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button class="btn btn-primary btn-sm" type="button">
+                    Add
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
       {members.length > 0 ? (
         <ul class="list-disc pl-5">
           {members.map((member) => (
